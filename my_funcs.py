223f8fd40cfbd80d0845ff463b4dde1b5d32f7bf
IgorReshetnyak <60608409+IgorReshetnyak@users.noreply.github.com>
2020-02-03 13:34:56 +0100

    Update my_funcs.py

--- my_funcs.py
+++ my_funcs.py
@@ -4,23 +4,6 @@ import math
 import numpy
 import time
 
-def smooth(x,window_len=11,window='hanning'):
-        if x.ndim != 1:
-                raise ValueError, "smooth only accepts 1 dimension arrays."
-        if x.size < window_len:
-                raise ValueError, "Input vector needs to be bigger than window size."
-        if window_len<3:
-                return x
-        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-                raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
-        s=numpy.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
-        if window == 'flat': #moving average
-                w=numpy.ones(window_len,'d')
-        else:  
-                w=eval('numpy.'+window+'(window_len)')
-        y=numpy.convolve(w/w.sum(),s,mode='same')
-        return y[window_len:-window_len+1]
-
 def broad_gauss(data, energies, broad=0.1): 
     if len(data)!=len(energies): 
      exit("Error data and energies length differ") 
@@ -79,7 +62,9 @@ def printStage(txt):
 
 def eelsToEps(spectra):
   n=len(spectra)
+
   if n<1: exit()
+
   if n%2==1: n=n-1
   im=spectra
   re=spectra 
@@ -94,17 +79,3 @@ def eelsToEps(spectra):
   e1 = re / (re ** 2 + im ** 2)
   e2 = im / (re ** 2 + im ** 2)    
   return e2
-
-
-    # d = d ./ rk; # Im(-1/eps)
-    # imreps = d; # stored value, not to be transformed
-    
-    # d = fft(d,nn); # Fourier transform      
-    # d = -2 .* imag(d) ./ nn; #Transfer sine coefficients to cosine locations:
-    # d(1:nn/2) = -d(1:nn/2);        
-    # d = fft(d,nn); # Inverse transform
-    
-    # re = real(d(2:nn));
-    # den = re .*re + imreps(2:nn) .*imreps(2:nn);     
-    # eps1 = re ./ den;
-    # eps2 = imreps(2:nn) ./ den;
